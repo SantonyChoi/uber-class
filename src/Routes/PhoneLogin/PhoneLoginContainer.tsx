c312c47f4570ada9a6eda0a03ebf56a6cc4c6a72
seunghoon <santony.choi@gmail.com>
2018-09-24 14:16:21 +0900

    Toastify

--- src/Routes/PhoneLogin/PhoneLoginContainer.tsx
+++ src/Routes/PhoneLogin/PhoneLoginContainer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { RouteComponentProps } from "react-router-dom";
+import { toast } from "react-toastify";
 import PhoneLoginPresenter from "./PhoneLoginPresenter";
 
 interface IState {
@@ -11,16 +12,46 @@ class PhoneLoginContainer extends React.Component<
   RouteComponentProps<any>,
   IState
 > {
-
   public state = {
     countryCode: "+82",
     phoneNumber: ""
-  }
+  };
 
   public render() {
-    const {countryCode, phoneNumber} = this.state;
-    return <PhoneLoginPresenter countryCode={countryCode} phoneNumber={phoneNumber} />;
+    const { countryCode, phoneNumber } = this.state;
+    return (
+      <PhoneLoginPresenter
+        countryCode={countryCode}
+        phoneNumber={phoneNumber}
+        onInputChange={this.onInputChange}
+        onSubmit={this.onSubmit}
+      />
+    );
   }
+
+  public onInputChange: React.ChangeEventHandler<
+    HTMLInputElement | HTMLSelectElement
+  > = event => {
+    const {
+      target: { name, value }
+    } = event;
+    this.setState({
+      [name]: value
+    } as any);
+  };
+
+  public onSubmit: React.FormEventHandler<HTMLFormElement> = event => {
+    event.preventDefault();
+    const { countryCode, phoneNumber } = this.state;
+    const isValid = /^\+[1-9]{1}[0-9]{7,11}$/.test(
+      `${countryCode}${phoneNumber}`
+    );
+    if (isValid) {
+      toast.info("Correct!");
+    } else {
+      toast.error("Please write a valid phone number");
+    }
+  };
 }
 
 export default PhoneLoginContainer;
